a55d99d3674904562d2ca4b4bc9201eb4ce22866
Dhruvil-Dave <dhruvildave45@gmail.com>
2021-08-01 14:35:31 +0530

    update

---
+++ src/Checkout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Checkout.css';
+import { useStateValue } from './StateProvider';
+import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
+
+function Checkout() {
+    const[{basket}, dispatch] = useStateValue();
+
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://disruptiveadvertising.com/static/c653234a6e43c8c1c2c33cb2ac5e4477/7f757/Screen-Shot-2018-10-29-at-11.50.03-AM.png"/>
+
+                <div>
+                    <h2 className="checkout__title">Your Shopping Basket</h2>
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id = {item.id}
+                            title = {item.title}
+                            image = {item.image}
+                            price = {item.price}
+                            rating = {item.rating}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <Subtotal/>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDEezXP2NgcP1SP0jmVcui2ylRf6pp9-eo",
+    authDomain: "challenge-80a0c.firebaseapp.com",
+    projectId: "challenge-80a0c",
+    storageBucket: "challenge-80a0c.appspot.com",
+    messagingSenderId: "379559067925",
+    appId: "1:379559067925:web:7fb5de5a491614bb31691c",
+    measurementId: "G-153Y2X2PBS"
+  };

cc17d4b7a185116a5c2f1d69cca4f1890413966e
Dhruvil-Dave <dhruvildave45@gmail.com>
2021-08-04 01:47:01 +0530

    update

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-    const[{basket}, dispatch] = useStateValue();
+    const[{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -13,6 +13,7 @@ function Checkout() {
                 <img className="checkout__ad" src="https://disruptiveadvertising.com/static/c653234a6e43c8c1c2c33cb2ac5e4477/7f757/Screen-Shot-2018-10-29-at-11.50.03-AM.png"/>
 
                 <div>
+                    <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout__title">Your Shopping Basket</h2>
                     {basket.map(item => (
                         <CheckoutProduct

efc0e7bf0774320ee93b0c5eea176334c38a0650
Dhruvil-Dave <dhruvildave45@gmail.com>
2021-08-03 17:55:36 +0530

    feat: login route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
     <div className="app">
     <Header/>
       <Switch>
+      <Route path="/login">
+            <h1>Login Page</h1>
+        </Route>
         <Route path="/checkout">
             <Checkout/>
         </Route>
